917bf902b21000a8e79e6b6710e39db67b4c1afa
tsmorz <tonysmoragiewicz@gmail.com>
2025-03-31 19:14:51 -0400

    initial commit

---
+++ __main__.py
@@ -0,0 +1,12 @@
+"""Basic docstring for my module."""
+
+from loguru import logger
+
+
+def main() -> None:
+    """Run a simple demonstration."""
+    logger.info("This is an empty repo")
+
+
+if __name__ == "__main__":
+    main()
