c0e7ac1bc7fcaa0c037761771be479f954b007e6
tsmorz <tonysmoragiewicz@gmail.com>
2025-04-01 13:43:29 -0400

    add test data and formatting

--- tests/math_utils_test.py
+++ tests/math_utils_test.py
@@ -9,7 +9,12 @@ from imu_fusion_py.math_utils import align_to_acceleration
 
 @pytest.mark.parametrize(
     "gravity_alignment_vector",
-    [np.array([1.0, 0.0, 0.0]), np.array([0.0, 1.0, 0.0]), np.array([0.0, 0.0, 1.0])],
+    [
+        np.array([0.0, 0.0, 1.0]),
+        np.array([0.0, 1.0, 0.0]),
+        np.array([0.0, 0.0, 1.0]),
+        np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0.0]),
+    ],
 )
 def test_align_to_gravity(gravity_alignment_vector) -> None:
     """Assert that align_to_gravity function returns a rotation matrix."""
@@ -20,4 +25,4 @@ def test_align_to_gravity(gravity_alignment_vector) -> None:
     rot = align_to_acceleration(acceleration_vec=acc_vector)
 
     # Assert
-    np.testing.assert_array_almost_equal(rot[2, :], gravity_alignment_vector)
+    np.testing.assert_array_almost_equal(rot[2, :], gravity_alignment_vector, decimal=3)
